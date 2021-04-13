2e3bc1e385cbf3789abb73e512fa39d7b00668bf
Joseph Lander <josephclander@gmail.com>
2021-04-13 17:49:35 +0100

    Add error handling

--- app.js
+++ app.js
@@ -1,22 +1,54 @@
 const express = require('express');
-const data = require('./data.json');
+const { projects } = require('./data.json');
 const path = require('path');
 const app = express();
+let PORT = 3000;
 
 app.set('view engine', 'pug');
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index', data.projects);
+  res.render('index', projects);
 });
 
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/project/:id', (req, res) => {
+app.get('/project/:id', (req, res, next) => {
   const projectID = req.params.id;
-  res.render('project', { data, projectID });
+  if (projects[projectID]) {
+    res.render('project', { projects, projectID });
+  } else {
+    // create error 404 if project not found
+    const err = new Error('The project is not found');
+    err.status = 404;
+    console.log(err.status, err.message);
+    next(err);
+  }
 });
 
-app.listen(3000, console.log('App listening on port 3000'));
+// path to test 500 server errors (as requested in instructions)
+app.get('/error', (req, res, next) => {
+  const err = new Error('This is a test server error');
+  err.status = 500;
+  console.log(err.status, err.message);
+  next(err);
+});
+
+// create error 404
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  console.log(err.status, err.message);
+  next(err);
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+app.listen(PORT, console.log(`App listening on port ${PORT}`));
